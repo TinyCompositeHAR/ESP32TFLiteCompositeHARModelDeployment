10c337690be3a2b6a34c9b5f8d2346eddc547921
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2024-09-27 16:55:05 +0600

    refactor: changed model name to a more meaningful one

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,29 @@
 // Needs min of 6k ish for it work properly
 #define ARENA_SIZE 6000
 
-Eloquent::TF::Sequential<TF_NUM_OPS, ARENA_SIZE> convMLP;
+Eloquent::TF::Sequential<TF_NUM_OPS, ARENA_SIZE> simpleMLP;
 
 void setup() {
     Serial.begin(115200);
     Serial.println("__TENSORFLOW MLP__");
     delay(3000);
 
-    while (!convMLP.begin(SimpleMLP).isOk())
-        Serial.println(convMLP.exception.toString());
+    while (!simpleMLP.begin(SimpleMLP).isOk())
+        Serial.println(simpleMLP.exception.toString());
 }
 
 
 void loop() {
-    if (!convMLP.predict(x20).isOk()) {
-        Serial.println(convMLP.exception.toString());
+    if (!simpleMLP.predict(x20).isOk()) {
+        Serial.println(simpleMLP.exception.toString());
         return;
     }
 
     Serial.print("expected class 20, predicted class ");
-    Serial.println(convMLP.classification);
+    Serial.println(simpleMLP.classification);
 
     Serial.print("It takes ");
-    Serial.print(convMLP.benchmark.microseconds());
+    Serial.print(simpleMLP.benchmark.microseconds());
     Serial.println("us for a single prediction");
     delay(1000);
 }
