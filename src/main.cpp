633e7f4c589d5b9b05774d1ac31596f61bff73e8
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2024-09-27 17:01:12 +0600

    revert: reverted back green zone

--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,33 @@
 #include "SimpleMLP.h"
 #include <tflm_esp32.h>
 #include <eloquent_tinyml.h>
-#include "esp_system.h"
-#include "esp32-hal-cpu.h"  // For CPU frequency
 
 // Needs min of 6k ish for it work properly
 #define ARENA_SIZE 6000
 
-Eloquent::TF::Sequential<TF_NUM_OPS, ARENA_SIZE> convMLP;
+Eloquent::TF::Sequential<TF_NUM_OPS, ARENA_SIZE> simpleMLP;
 
 void setup() {
     Serial.begin(115200);
     Serial.println("__TENSORFLOW MLP__");
     delay(3000);
 
-    while (!convMLP.begin(SimpleMLP).isOk())
-        Serial.println(convMLP.exception.toString());
+    while (!simpleMLP.begin(SimpleMLP).isOk())
+        Serial.println(simpleMLP.exception.toString());
 }
 
-void loop() {
-    // Capture start time
-    uint32_t startTime = micros();
 
-    if (!convMLP.predict(x20).isOk()) {
-        Serial.println(convMLP.exception.toString());
+void loop() {
+    if (!simpleMLP.predict(x20).isOk()) {
+        Serial.println(simpleMLP.exception.toString());
         return;
     }
 
-    // Measure CPU frequency (in MHz)
-    uint32_t cpuFreq = getCpuFrequencyMhz();
-
-    // Calculate elapsed time for prediction
-    uint32_t elapsedTime = micros() - startTime;
-
-    // Print the prediction result
     Serial.print("expected class 20, predicted class ");
-    Serial.println(convMLP.classification);
+    Serial.println(simpleMLP.classification);
 
-    // Print the time taken for prediction
     Serial.print("It takes ");
-    Serial.print(elapsedTime);
-    Serial.println(" us for a single prediction");
-
-    // Print CPU frequency
-    Serial.print("CPU Frequency: ");
-    Serial.print(cpuFreq);
-    Serial.println(" MHz");
-
-    // Print free heap memory
-    Serial.print("Free heap memory: ");
-    Serial.print(esp_get_free_heap_size());
-    Serial.println(" bytes");
-
+    Serial.print(simpleMLP.benchmark.microseconds());
+    Serial.println("us for a single prediction");
     delay(1000);
 }
